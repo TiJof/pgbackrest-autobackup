2a71eb307be19905e617f243bee3ccb0432747ef
Geoffroy RABOUIN <grabouin@iris-network.fr>
2018-10-02 10:20:17 +0200

    add /etc/pgbackrest/conf.d/ add default config add command to restore standby from pgbackrest

--- BackupPg.sh
+++ BackupPg.sh
@@ -2,20 +2,35 @@
 
 # Configuration pgbackrest - /etc/pgbackrest.conf
 ## [global]
+## compress=n
+##
 ## repo1-path=/var/lib/pgbackrest
-## process-max=4
-## stop-auto=y
-## archive-check=y
-## repo1-retention-full=1
-## repo1-retention-diff=8
+## repo1-retention-full=2
+## repo1-retention-diff=6
 ##
-## [localhost]
-## pg1-path=/var/lib/postgresql/10/main
+## backup-standby=y
+##
+## process-max=2
+##
+## archive-async=y
+## spool-path=/var/spool/pgbackrest
+## archive-push-queue-max=33M
+## compress-level-network=3
+##
+## [main]
 ## pg1-port=5432
-##
-## [localhost2]
-## pg1-path=/var/lib/postgresql/10/test42
-## pg1-port=5433
+## pg1-path=/var/lib/postgresql/10/main
+## pg1-host=172.25.105.144
+## pg2-port=5432
+## pg2-path=/var/lib/postgresql/10/main
+## pg2-host=172.25.105.193
+## pg3-port=5432
+## pg3-path=/var/lib/postgresql/10/main
+## pg3-host=172.25.105.170
+
+# Restauration depuis le pgbackrest
+## sudo -u postgres pgbackrest --stanza=main restore --pg1-path=/var/lib/postgresql/10/main --recovery-option=standby_mode=on --recovery-option=primary_conninfo='host=172.25.105.144 port=5432 user=postgres'
+
 
 # Binary files
 PgBackRest=/usr/bin/pgbackrest
@@ -27,12 +42,14 @@ Jq=/usr/bin/jq
 
 # Variables
 PgUser=postgres
-PgBackRestConf=/etc/pgbackrest.conf
+PgBackRestConf=/etc/pgbackrest/pgbackrest.conf
+PgBackRestConfDir=/etc/pgbackrest/conf.d/
 DirBackup=/var/lib/pgbackrest/backup
 LogLevel=info
 ExitCode=0
+WarnTime="1 day"
 
-for Stanza in $(${Grep} -P '(?!.*global)^\[' ${PgBackRestConf} | ${Tr} -d [] | tr '\n' ' ') ; do
+for Stanza in $(${Grep} -P '(?!.*global)^\[' ${PgBackRestConf} | ${Tr} -d [] | tr '\n' ' ') $(${Grep} -P '(?!.*global)^\[' ${PgBackRestConfDir}/* | ${Tr} -d [] | tr '\n' ' ') ; do
   # Let's verify if the stanza is created
   if [ ! -d ${DirBackup}/${Stanza} ] ; then
     # If not the case, create them
@@ -53,7 +70,7 @@ for Stanza in $(${Grep} -P '(?!.*global)^\[' ${PgBackRestConf} | ${Tr} -d [] | t
     monit)
       LastBackup=$(${Sudo} -u ${PgUser} ${PgBackRest} --output=json --stanza=${Stanza} info | ${Jq} '.[0] | .backup[-1] | .timestamp.stop')
       echo Last backup was at $(${Date} -d @${LastBackup})
-      if [ ${LastBackup} -lt $(${Date} +%s -d '-1 day') ] ; then echo Backup is too old ; ExitCode=2 ; fi
+      if [ ${LastBackup} -lt $(${Date} +%s -d "-${WarnTime}") ] ; then echo Backup is too old ; ExitCode=2 ; fi
       continue
       ;;
     "")
